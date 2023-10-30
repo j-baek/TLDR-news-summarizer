c5a172a1680eda1bf03c2ad4a5f3d07db0b57016
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-10-30 13:30:21 +0000

    added a feature to select news from title and see the summary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import textSoup as news_data
+import textSoup as data_function
 from flask import Flask, render_template
 import json
 
@@ -15,20 +15,33 @@ if __name__ == "__main__" :
     try:
         with open('news_url.json', 'r') as file:
             url_data = json.load(file)
+            
     except FileNotFoundError:
         print("json file not found!!!")
 
     file.close()
+    num_id_len = len(url_data)
+    
+    for i in range(num_id_len) :
+        if url_data[i].get("id") == str(i + 1):
+            news_json = data_function.get_data(url_data[i]["url"])
+            print("\n " + str(i + 1))
+            print("Title : " + news_json["title"] + "\n")
+    
+    userPick = input("Pick a news to see the summary. Please enter a number: ")
+    print("\n")
 
-    for item in url_data:
-        for i in range(10) :
-            if item.get('id') == str(i + 1):
-                news_json = news_data.get_data(item["url"])
-                print("\n " + str(i + 1) + "\n ")
-                print("Title : " + news_json["title"] + "\n")
-                print(news_json["summary"])
-                print("\n\n")
+    NEWS_FOUND = False
+    for i in range(num_id_len) :
+        if url_data[i].get("id") == userPick:
+            news_json = data_function.get_data(url_data[i]["url"])
+            print("Title: " + news_json["title"] + "\n")
+            print("\n" + news_json["summary"]+ "\n")
+            NEWS_FOUND = True
+            break;
 
+    if NEWS_FOUND is False :
+        print("Invalid input. \n")
 
 else:
     print(" imported")
