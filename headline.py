738087e5087ca79991048fbfbb97f3119be3fe99
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-01 13:47:32 +0000

    in progress of working with headline and its url

--- headline.py
+++ headline.py
@@ -3,16 +3,43 @@ import requests
 
 url = "https://edition.cnn.com/business/tech"
 
+'''
+def make_headline_json():
+    response = requests.get(url, timeout=10)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        headlines = soup.find_all('span', {"class" : "container__headline-text"})
+        headlineurls = soup.find_all('a', {"class": "container__link container__link--type-article container_lead-plus-headlines-with-images__link"}, {"data-link-type": "article"})
+
+        # make python dictionary containing headline and its url
+        h_data = {}
+        
+        # count of id
+        c = 1
+        for head_line in headline_urls:
+            h_data["id"] = str(c)
+            h_data["headline"] = head_line
+
+
+
+
+    else: print("request not SUCCESSFUL!")
+'''
+
 def get_headlines():
     response = requests.get(url, timeout=10)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
 
         head_lines = soup.find_all('span', {"class" : "container__headline-text"})
-        #print(head_lines)
-        
+        # set to check headline duplications
+        h_set = set()
         for head_line in head_lines:
-            print(head_line.text)
+            dup = head_line in h_set
+            if(not dup):
+                print(head_line.text)
+                h_set.add(head_line)
     else:
         print("reqeust not SUCCESSFUL!!!")
 
@@ -22,24 +49,14 @@ def get_headline_url():
         soup = BeautifulSoup(response.content, 'html.parser')
 
         headline_urls = soup.find_all('a', {"class": "container__link container__link--type-article container_lead-plus-headlines-with-images__link"}, {"data-link-type": "article"})
-        
+        prev_url = "https://edition.cnn.com" 
         for headline_url in headline_urls:
+            # the url doesn't included https://edition.cnn.com so add them manually
             h_url = "https://edition.cnn.com" + headline_url.get('href')
+            # there are some duplications so exclude them
+            if prev_url != h_url:
+                print(h_url)
+            prev_url = h_url
 
-            print(h_url)
-
-#get_headlines()
-get_headline_url()
-
-
-'''
-<a href="/2023/10/31/tech/canada-china-wechat-ban-security-hnk-intl/index.html" class="container__link container__link--type-article container_lead-plus-headlines-with-images__link"
-        data-link-type="article"
-    >
-                <div class="container__text container_lead-plus-headlines-with-images__text">
-            <div class="container__headline  container_lead-plus-headlines-with-images__headline">
-                    <span class="container__headline-text" data-editable="headline">Canada bans China’s Wechat from government devices citing security risks</span>
-            </div>
-        </div>
-    </a>
-    '''
+get_headlines()
+#get_headline_url()
