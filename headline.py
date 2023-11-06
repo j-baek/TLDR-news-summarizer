d3b84cc7b7572995643c0b3393cde26c71a7e5b1
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-06 14:01:27 +0000

    wrote get_today_news function. It stores today's news data into today_news.json file.

--- headline.py
+++ headline.py
@@ -5,12 +5,14 @@ from datetime import datetime
 import os
 import json
 
-# put headline page url as input and it adds json objects to headline_url.json file
-def get_headline_url(url):
+# get today's news and store them in today_news.json file
+def get_today_news(url):
     response = requests.get(url, timeout=10)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         headline_urls = soup.find_all('a', {"class": "container__link container__link--type-article container_lead-plus-headlines-with-images__link"}, {"data-link-type": "article"})
+        # for CNN news source code, there are some duplicated urls in a row
+        # to prevent getting duplicated urls, compare  previous url and current url
         prev_url = "https://edition.cnn.com" 
         TODAY_NEWS = False
         EMPTY_FILE = True
@@ -26,32 +28,46 @@ def get_headline_url(url):
                     # first, delete previous data if the data is not today's data
                     if TODAY_NEWS == False:
                         try:
-                            open('today_news.json', 'w').close()
+                            # using 'w' mode wipes the whole data in the file to write new data
+                            # so opening the file with 'w' and closing the file will just wipe the whole memory
+                            open('today_news.json', 'w').close() 
                         except FileNotFoundError as error:
                             print(f"An error occurred while working with the file!!!: {str(error)}")
                         TODAY_NEWS = True
                     # write today's news data 
                     else:
+                        existing_data ="" # if this variable doesn't store data, don't write it in the json file
                         
-                        try:
-                            with open('today_news.json', 'r') as file:
-                                if EMPTY_FILE == False: # if it is an empty file, json.load will throw an exception error
+                        if EMPTY_FILE == False: # if it is an empty file, json.load function will throw an exception error so check it
+                            try:
+                                with open('today_news.json', 'r') as file:
                                     existing_data = json.load(file)
-                                file.close()
-                        except FileNotFoundError as error:
-                            print(f"An error occurred while working with the file!!!: {str(error)}")
-                        if EMPTY_FILE == True:
-                            with open('today_news.json', 'w') as file:
-                                json.dump(data, file, indent=4)
-                                EMPTY_FIle = False
-                                file.close()
-                        else:
-                            existing_data.append(data)
-                            with open('today_news.json', 'w') as file:
-                                json.dump(existing_data, file, indent=4)
-                                file.close()
+                                    file.close()
+                            except FileNotFoundError as error:
+                                print(f"An error occurred while working with the file!!!: {str(error)}")
+                        else: # if writing the first news in the empty file
+                            try:
+                                with open('today_news.json', 'w') as file:
+                                    print("im empty file!")
+                                    jsonfile = [] # create a list and put python dictionary within the list. Otherwise, the dictionaries will not be within '[]'
+                                    jsonfile.append(data)
+                                    json.dump(jsonfile, file, indent=4)
+                                    
+                                    EMPTY_FILE = False
+                                    file.close()
+                            except FileNotFoundError as error:
+                                print(f"An error occurred while working with the file!!!: {str(error)}")
                         
-
+                        if len(existing_data) != 0: # when there is a data in existing_data
+                            try:
+                                with open('today_news.json', 'w') as file:
+                                    existing_data.append(data)
+                                    json.dump(existing_data, file, indent=4)
+                                    file.close()
+                            except FileNotFoundError as error:
+                                print(f"An error occurred while working with the file!!!: {str(error)}")
+                        
+                # set current url to be previous url after processing it
                 prev_url = h_url
         else:
             print("today_news.json file not FOUND!!!")
@@ -85,6 +101,6 @@ def check_last_headline_update(url):
     # if the headline has not been updated, update it 
     if today != last_date_called:
         print("New data processing")
-        get_headline_url(url)
+        get_today_news(url)
     else:
         print("Today's headline already acquired\n\n\n")
