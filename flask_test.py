d61130d31dc6e473c9e8c4bf1cd1e262696ae3bd
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-08 14:26:52 +0000

    testing flask

--- flask_test.py
+++ flask_test.py
@@ -4,19 +4,10 @@ import textSoup as data_function
 app = Flask(__name__)
 
 @app.route('/')
-
-def news_summary():
-    try:
-        with open('news_data.json', 'r') as file:
-            news_data = json.load(file)
-            file.close()
-    except FileNotFoundError:
-        print("json file not FOUND")
-    
-    summary = news_data[14]["title"] + "\n" + "\n" + news_data[14]["summary"]
-    return summary
+def home():
+    return render_template("index.html")
     
     
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
